92674cb56bff584f13b8c09aa1e3a18987ceaab0
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-10-07 23:26:55 +0000

    Front end: made over

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+var icons, number;
+
 window.onload = function() {
   //write your code here
   console.log("This is the Cards Generator app!");
+  // recoge las variables
+  icons = document.getElementsByTagName("card-suit");
+  number = document.getElementsByClassName("card-number");
 };
+
+function generateCard() {
+  // suit => { ♦ ♥ ♠ ♣ }
+  var suit = Math.floor(Math.random() * 4) + 1;
+  var number = Math.floor(Math.random() * 13) + 1;
+}
